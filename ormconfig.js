20eb7729facf101117640c0e6809a00e8121ee49
Tohn Matos <tohn.matos.ext@talentfour.com.br>
2020-12-08 18:35:00 -0300

    chore: log queries

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,7 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  logging: false,
+  logging: true,
   migrations: [`src/migrations/*.{js,ts}`],
   entities: [`src/models/*.{js,ts}`],
   cli: {
